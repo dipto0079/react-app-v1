91da2a64c2f83ed8439051f23968944596b55e32
dipto0079 <sudiptobiswas.coder@gmail.com>
2022-03-20 01:13:06 +0600

    fa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/Expenseltem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
