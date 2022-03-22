04acfbf03f8255d51af3bf7e918b60d93f7541c1
dipto0079 <sudiptobiswas.coder@gmail.com>
2022-03-22 23:25:44 +0600

    16 video

--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,23 +1,15 @@
-import './Expenseltem.css';
+import "./Expenseltem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props){
-
-const month = props.date.toLocaleString('en-US',{month:'long'});
-const date = props.date.toLocaleString('en-US',{day:'2-digit'});
-const year = props.date.getFullYear();
-
-    return(
-        <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{date}</div>
-            </div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
